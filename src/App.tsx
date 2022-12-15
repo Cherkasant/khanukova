27ab3173a0cd0115301f122413449db82a978e88
Olga Grishchenko <104859965+OlgaGrishchenko@users.noreply.github.com>
2022-12-15 20:18:15 +0300

    add components Button (#1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import "./App.css";
+import Button, { ButtonTypes } from "./Components/Button";
+import { GoogleIcon } from "./Assets/icons/GoogleIcon";
+import { LinkedinIcon } from "./Assets/icons/LinkedinIcon";
 import Role from "./Components/Role";
+import { ProjectsIcon } from "./Assets/icons/ProjectsIcon";
 
 function App() {
   return (
     <div className="App">
       <Role name={"Test"} />
+      <Button title={"Login"} type={ButtonTypes.TextButton}/>
+      <Button title={<LinkedinIcon />} type={ButtonTypes.IconButton} />
+      <Button title={"Login"} type={ButtonTypes.TextButton} disabled={true}/>
+      <ProjectsIcon />
     </div>
   );
 }
