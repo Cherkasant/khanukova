c566cce6b23c5303675e671702c7b7e09c77531d
LeonidZhukovets <leonid.zhukovets@gmail.com>
2022-12-15 23:33:09 +0300

    add Card Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import "./App.css";
 import Button, { ButtonTypes } from "./Components/Button";
-import { GoogleIcon } from "./Assets/icons/GoogleIcon";
 import { LinkedinIcon } from "./Assets/icons/LinkedinIcon";
 import Role from "./Components/Role";
 import { ProjectsIcon } from "./Assets/icons/ProjectsIcon";
+import Card from "./Components/Card";
 
 function App() {
+  const MOCK_CARD = {
+    id: 1,
+    title: "Рога и Копыта",
+    tasksProgress: "20%",
+    date: "16.02.2222",
+    budget: "1000000$",
+    payment: "12500$",
+  };
+
   return (
     <div className="App">
       <Role name={"Test"} />
-      <Button title={"Login"} type={ButtonTypes.TextButton}/>
+      <Button title={"Login"} type={ButtonTypes.TextButton} />
       <Button title={<LinkedinIcon />} type={ButtonTypes.IconButton} />
-      <Button title={"Login"} type={ButtonTypes.TextButton} disabled={true}/>
+      <Button title={"Login"} type={ButtonTypes.TextButton} disabled={true} />
       <ProjectsIcon />
+      <Card card={MOCK_CARD} />
     </div>
   );
 }
---
+++ src/Components/constants/@types.ts
@@ -0,0 +1,9 @@
+//todo уточнить со стороны бэкенда
+export type CardType = {
+  id: number;
+  title: string;
+  tasksProgress: string;
+  date: string;
+  budget: string;
+  payment: string;
+};
