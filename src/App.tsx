a48ce5241886cb759a3f980d0c9ebb50495ae589
Anna.Rubchenia <anna.rub4enya@yandex.ru>
2022-12-15 19:45:35 +0100

    added input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Role from "./Components/Role";
+import Input from "./Components/Input";
+import onChangeInput from "./Components/Input";
+import InputProps from "./Components/Input";
 
 function App() {
+  const [inputValue, setInputValue] = useState(" ");
+  const onChange = (value: string) => {
+    setInputValue(value);
+  };
   return (
     <div className="App">
       <Role name={"Test"} />
+      <Input value={inputValue} onChange={onChange}></Input>
     </div>
   );
 }
