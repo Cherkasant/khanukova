4e16aa5ccd7110556ad31f2d8dd3a52b76dc5763
OlgaGrishchenko <olg.grishchenko@gmail.com>
2023-01-03 15:13:39 +0300

    update component Menu

--- src/Components/Header/Menu/Menu.tsx
+++ src/Components/Header/Menu/Menu.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { ChatsIcon } from "../../../Assets/icons/ChatsIcon";
 
@@ -8,35 +8,65 @@ import { ProjectsIcon } from "../../../Assets/icons/ProjectsIcon";
 import { LibraryIcon } from "../../../Assets/icons/LibraryIcon";
 import { PaymentsIcon } from "../../../Assets/icons/PaymentsIcon";
 import { NotificationsIcon } from "../../../Assets/icons/NotificationsIcon";
+import { ArrayDownIcon } from "../../../Assets/icons/ArrayDownIcon";
+import { AddNewProjectIcon } from "../../../Assets/icons/AddNewProjectIcon";
 
 const UserMenu = () => {
 
   const { pathname } = useLocation();
+
   const navButtons = [
     { name: "Chats", icon: <ChatsIcon />, link: "/" },
-    { name: "Projects", icon: <ProjectsIcon />, link: "" },
+    { name: "Projects", icon: <ProjectsIcon />, link: "", button: <ArrayDownIcon />, active: true },
     { name: "Library", icon: <LibraryIcon />, link: "" },
     { name: "Payments", icon: <PaymentsIcon />, link: "" },
     { name: "Notifications", icon: <NotificationsIcon />, link: "" },
   ];
 
+  const [isOpened, setOpened] = useState(false);
+
+  console.log(isOpened)
+
   return (
     <>
     <div className={styles.container}>
-      {navButtons.map(({ link, name, icon }) => {
-        return (
+
+      {navButtons.map(({ link, name, icon, button, active }) =>
+      
+        <div>
+
           <NavLink
+
             key={link}
             to={link}
             className={classNames(styles.navButton, {
               [styles.activeNavButton]: pathname === link,
+              [styles.new]: name === "Projects"
             })}
+            
           >
-            {icon}
-            {name}
+            <div>
+
+            <div className={styles.div}>
+              {icon}
+              {name}
+              <div
+              onClick={() => setOpened(!isOpened)}
+              >{button}</div>
+            </div>
+
+            {active? <div className={classNames(styles.list, {[styles.listActive]:isOpened})}>
+              <div className={styles.el}> <div><AddNewProjectIcon /></div>New project</div>
+              </div> : null}
+            </div>
+            
+            
+
           </NavLink>
-        );
-      })}
+
+        </div>)}
+
+      
     </div>
   </>
 );
