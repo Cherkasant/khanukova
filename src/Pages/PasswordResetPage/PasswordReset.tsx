b81d94aaa43dee599056aea1ac4c7ad63211ccbe
Anna.Rubchenia <anna.rub4enya@yandex.ru>
2023-01-06 18:43:25 +0100

    added PasswordResetPage

---
+++ src/Pages/PasswordResetPage/PasswordReset.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import styles from "./PasswordReset.module.css";
+import classNames from "classnames";
+import Title from "../../Components/Title";
+import Input from "../../Components/Input";
+import Button from "../../Components/Button";
+import { ButtonTypes } from "../../Components/Button";
+
+const ResetPassword = () => {
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.title}>
+          <Title name={"Reset password"}></Title>
+          <div className={styles.description}>
+            {"Please enter your new password"}
+          </div>
+        </div>
+
+        <div className={styles.inputsContainer}>
+          <Input
+            placeholder={"New password "}
+            value={password}
+            onChange={(value: string) => setPassword(value)}
+          />
+          <Input
+            placeholder={"Confirm Password "}
+            value={passwordConfirm}
+            onChange={(value: string) => setPasswordConfirm(value)}
+          />
+
+          <Button
+            title={"Save new password"}
+            type={ButtonTypes.TextButton}
+            onClick={() => {}}
+            className={styles.button}
+          />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ResetPassword;
