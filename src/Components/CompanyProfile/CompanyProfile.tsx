9cb9753692a599b2e9749b7a5bdc27e79d38cf3e
Cherkasant <antnchrks@gmail.com>
2023-02-20 18:07:46 +0300

    added Payments

--- src/Components/CompanyProfile/CompanyProfile.tsx
+++ src/Components/CompanyProfile/CompanyProfile.tsx
@@ -1,40 +1,19 @@
-import React, { FC, useState } from "react";
+import React, { FC } from "react";
 import { CompanyList } from "../constants/@types";
 import styles from "./CompanyProfile.module.css";
-import { EditTitleIcon } from "../../Assets/icons/EditTitleIcon";
+import CompanyProfileCard from "../CompanyProfileCard";
 
 type CompanyProfileProps = {
   activeTab?: CompanyList;
   disabled?: boolean;
   CompanyList: Array<{ name: string; key: CompanyList; answers: string }>;
-  onChangeList?: (value: string) => void;
 };
 
 const CompanyProfile: FC<CompanyProfileProps> = ({ CompanyList }) => {
-  const [edit, setEdit] = useState(false);
-  const onEditClick = () => {
-    setEdit(!edit);
-  };
-
   return (
     <div className={styles.listContainer}>
-      {CompanyList.map((list) => {
-        return (
-          <div key={list.name} className={styles.list}>
-            {list.key}
-            <textarea
-              key={list.answers}
-              name={"text"}
-              wrap={"soft"}
-              className={styles.input}
-              defaultValue={list.answers}
-              disabled={edit}
-            />
-            <div className={styles.icon} onClick={onEditClick}>
-              <EditTitleIcon />
-            </div>
-          </div>
-        );
+      {CompanyList.map((card) => {
+        return <CompanyProfileCard CardType={card} key={card.key} />;
       })}
     </div>
   );
