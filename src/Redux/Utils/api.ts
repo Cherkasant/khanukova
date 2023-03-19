dd9c2ebd31845b2fc749cfa14871a310217ca6e3
Cherkasant <antnchrks@gmail.com>
2023-03-19 10:40:05 +0500

    changed singIn with JWT

--- src/Redux/Utils/api.ts
+++ src/Redux/Utils/api.ts
@@ -1,144 +1,140 @@
-import { create } from "apisauce";
+import { create } from 'apisauce'
 import {
-  ActivateUserData,
-  RegisterHeadData,
-  RegisterPoData,
-  RegisterUserData,
-  ResetPasswordData,
-  SignInUserData,
-} from "../Types/auth";
-import { TaskType } from "../Types/tasks";
+	ActivateUserData,
+	RegisterHeadData,
+	RegisterPoData,
+	RegisterUserData,
+	ResetPasswordData,
+	SignInUserData
+} from '../Types/auth'
+import { TaskType } from '../Types/tasks'
 
-const JWT_TOKEN = "Token 2b5698f59e13ef3d6535bf15c4016e57dcb530f9";
-const JWT_TOKEN_PO = "Token 4fe1657489f4e420554f33120f98cf64957103f2";
-const JWT_TOKEN_HEAD = "Token 1e4b18fae204857eca1a4f110fc87b1c772b6c35";
+const JWT_TOKEN = 'Token cc55ec64983887bb2e985d9f408085447eb850e9'
+const JWT_TOKEN_PO = 'Token 4fe1657489f4e420554f33120f98cf64957103f2'
+const JWT_TOKEN_HEAD = 'Token 1e4b18fae204857eca1a4f110fc87b1c772b6c35'
 
-const API = create({ baseURL: " https://apipuzzle-be.herokuapp.com" });
+const API = create({ baseURL: ' https://apipuzzle-be.herokuapp.com' })
 
 const registerUser = (data: RegisterUserData) => {
-  return API.post("/auth/users/", data, {
-    headers: {
-      Authorization: JWT_TOKEN,
-    },
-  });
-};
+	return API.post('/auth/users/', data, {
+		headers: {
+			Authorization: JWT_TOKEN
+		}
+	})
+}
 
 const registerHeadInfo = (data: RegisterHeadData) => {
-  return API.post("/user-profile/head-company/", data, {
-    headers: {
-      Authorization: JWT_TOKEN,
-    },
-  });
-};
+	return API.post('/user-profile/head-company/', data, {
+		headers: {
+			Authorization: JWT_TOKEN
+		}
+	})
+}
 const registerPoInfo = (data: RegisterPoData) => {
-  return API.post("/user-profile/po-company/", data, {
-    headers: {
-      Authorization: JWT_TOKEN,
-    },
-  });
-};
+	return API.post('/user-profile/po-company/', data, {
+		headers: {
+			Authorization: JWT_TOKEN
+		}
+	})
+}
 
 const activateUser = (data: ActivateUserData) => {
-  return API.post("/auth/users/activation/", data);
-};
+	return API.post('/auth/users/activation/', data)
+}
 
 const signInUser = (data: SignInUserData) => {
-  return API.get("/auth/login/", data, {
-    headers: {
-      Authorization: JWT_TOKEN,
-    },
-  });
-};
+	return API.post('/auth/jwt/create/', data)
+}
 
 const sendResetEmail = (email: string) => {
-  return API.post(
-    "auth/users/reset_password/",
-    { email },
-    {
-      headers: {
-        Authorization: JWT_TOKEN,
-      },
-    }
-  );
-};
+	return API.post(
+		'auth/users/reset_password/',
+		{ email },
+		{
+			headers: {
+				Authorization: JWT_TOKEN
+			}
+		}
+	)
+}
 
 const resetPasswordConfirm = (data: ResetPasswordData) => {
-  return API.post("/auth/users/reset_password_confirm/", data, {
-    headers: {
-      Authorization: JWT_TOKEN,
-    },
-  });
-};
+	return API.post('/auth/users/reset_password_confirm/', data, {
+		headers: {
+			Authorization: JWT_TOKEN
+		}
+	})
+}
 
 const postMilestone = (milestone: TaskType) => {
-  return API.post("/project/milestone/", milestone, {
-    headers: {
-      Authorization: "Token 1e4b18fae204857eca1a4f110fc87b1c772b6c35",
-    },
-  });
-};
+	return API.post('/project/milestone/', milestone, {
+		headers: {
+			Authorization: 'Token 1e4b18fae204857eca1a4f110fc87b1c772b6c35'
+		}
+	})
+}
 
-const getMilestone = (page?: number) => {
-  return API.get("/project/milestone/", page, {
-    headers: {
-      Authorization: "Token 1e4b18fae204857eca1a4f110fc87b1c772b6c35",
-    },
-  });
-};
+const getMilestone = (token: string, page?: number) => {
+	return API.get(
+		'/project/milestone/',
+		{ page },
+		{ headers: { Authorization: `Bearer ${token}` } }
+	)
+}
 
 const postProjectTitle = (title: string) => {
-  return API.post("/project", title, {
-    headers: {
-      Authorization: "Token 1e4b18fae204857eca1a4f110fc87b1c772b6c35",
-    },
-  });
-};
+	return API.post('/project', title, {
+		headers: {
+			Authorization: 'Token 1e4b18fae204857eca1a4f110fc87b1c772b6c35'
+		}
+	})
+}
 
 const verifyToken = (token: string) => {
-  return API.post("/auth/jwt/verify/", { token });
-};
+	return API.post('/auth/jwt/verify/', { token })
+}
 const getNewAccessToken = (refresh: string) => {
-  return API.post("/auth/jwt/refresh/", { refresh });
-};
+	return API.post('/auth/jwt/refresh/', { refresh })
+}
 
 const getHeadCompanyList = (page?: number) => {
-  return API.get("/user-profile/head-company/", page, {
-    headers: {
-      Authorization: JWT_TOKEN_HEAD,
-    },
-  });
-};
+	return API.get('/user-profile/head-company/', page, {
+		headers: {
+			Authorization: JWT_TOKEN_HEAD
+		}
+	})
+}
 
 const editHeadCompanyList = (id: string) => {
-  return API.patch(`/user-profile/head-company/${id}/`, {
-    headers: {
-      Authorization: JWT_TOKEN_HEAD,
-    },
-  } )
+	return API.patch(`/user-profile/head-company/${id}/`, {
+		headers: {
+			Authorization: JWT_TOKEN_HEAD
+		}
+	})
 }
 
 const getECaseList = (page?: number) => {
-  return API.get("/user-profile/po-company/", page, {
-    headers: {
-      Authorization: JWT_TOKEN_PO,
-    },
-  });
-};
+	return API.get('/user-profile/po-company/', page, {
+		headers: {
+			Authorization: JWT_TOKEN_PO
+		}
+	})
+}
 
 export default {
-  registerUser,
-  sendResetEmail,
-  activateUser,
-  signInUser,
-  resetPasswordConfirm,
-  postMilestone,
-  getMilestone,
-  getNewAccessToken,
-  verifyToken,
-  postProjectTitle,
-  registerHeadInfo,
-  registerPoInfo,
-  getHeadCompanyList,
-  editHeadCompanyList,
-  getECaseList,
-};
+	registerUser,
+	sendResetEmail,
+	activateUser,
+	signInUser,
+	resetPasswordConfirm,
+	postMilestone,
+	getMilestone,
+	getNewAccessToken,
+	verifyToken,
+	postProjectTitle,
+	registerHeadInfo,
+	registerPoInfo,
+	getHeadCompanyList,
+	editHeadCompanyList,
+	getECaseList
+}
