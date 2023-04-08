4b4ee7b6d62e7c54520fafe73b13e0ee512cda6e
Cherkasant <antnchrks@gmail.com>
2023-04-08 22:50:46 +0500

    fix bugs

--- src/Pages/ProjectScreen/ProjectScreen.tsx
+++ src/Pages/ProjectScreen/ProjectScreen.tsx
@@ -22,7 +22,6 @@ import Tab from '../../Components/Tabs';
 import { Tabs } from '../../Components/constants/@types';
 import { postProject, setFilterVisible, setSelectedModalVisible } from '../../Redux/Reducers/postReducer';
 import postSelector from '../../Redux/Selectors/postSelector';
-
 import styles from './ProjectScreen.module.css';
 
 const TABS_NAMES = [
