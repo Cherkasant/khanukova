7e8f7b73ba988508e3814b2d80d6b5a4fe9bfb76
Leonid.Zhukovets <leonid.zhukovets@gmail.com>
2023-04-19 13:57:25 +0300

    Start fixed CheckBox

---
+++ src/Components/PuzzleCheckbox/PuzzleCheckbox.tsx
@@ -0,0 +1,20 @@
+import type { CheckboxChangeEvent } from 'antd/es/checkbox';
+
+import React from 'react';
+
+import { Checkbox } from 'antd';
+
+import styles from './PuzzleCheckbox.module.css';
+
+const onChange = (e: CheckboxChangeEvent) => {
+  console.log(`checked = ${e.target.checked}`);
+};
+
+const PuzzleCheckbox = () => {
+  return (
+    <div className={styles.container}>
+      <Checkbox onChange={onChange} />
+    </div>
+  );
+};
+export default PuzzleCheckbox;
