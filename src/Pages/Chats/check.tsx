76c20b9a3b92b8fcebb00e392bef7817b525532d
evgKireev <bob1_k@mail.ru>
2023-04-24 12:44:13 +0300

    check slint

---
+++ src/Pages/Chats/check.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useState } from 'react';
+
+import { Collapse } from 'antd';
+
+import classNames from 'classnames';
+
+import { useSelector } from 'react-redux';
+
+import Input from '../../Components/Input';
+import InputChat from '../../Components/Chats/InputChat';
+
+import styles from './Chats.module.css';
+
+export const Check = () => {
+  const [d, setD] = useState('');
+  return (
+    <div className={classNames(styles.e)}>
+      <Collapse />
+      <Input />
+      <InputChat />
+    </div>
+  );
+};
