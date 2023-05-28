6a13ea41e1b141f7093ac10b1ba2c5fd47738963
LeonidZhukovets <leonid.zhukovets@gmail.com>
2023-05-28 21:53:59 +0300

    Fixed dropdown

---
+++ src/Components/PuzzleDropdown/PuzzleDropdown.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './PuzzleDropdown.module.css';
+
+const PuzzleDropdown = () => {
+  return <div className={styles.container}></div>;
+};
+
+export default PuzzleDropdown;
