1dc22b00fed05b5306187068f3b1a2d773acd640
Eugene-Kucherov <eugku14@gmail.com>
2023-08-17 15:43:30 +0300

    date-picker

--- src/Components/Modals/ModalCalendar/EventTime/EventTime.tsx
+++ src/Components/Modals/ModalCalendar/EventTime/EventTime.tsx
@@ -1,5 +1,11 @@
 import moment from 'moment';
+import DatePicker, { ReactDatePickerCustomHeaderProps } from 'react-datepicker';
+import { IconButton } from '@mui/material';
 
+import UpArrowIcon from '../../../../Assets/icons/UpArrowIcon';
+import DownArrowIcon from '../../../../Assets/icons/DownArrowIcon';
+
+import 'react-datepicker/dist/react-datepicker.css';
 import styles from './EventTime.module.css';
 
 interface EventTimeProps {
@@ -17,12 +23,44 @@ const EventTime = ({ start, end }: EventTimeProps) => {
   const startTime = formatTime(start, moment(start).minutes() === 0 ? 'h A' : 'h:mm A');
   const endTime = formatTime(end, moment(end).minutes() === 0 ? 'h A' : 'h:mm A');
 
+  const CustomHeader = ({ date, decreaseMonth, increaseMonth }: ReactDatePickerCustomHeaderProps) => {
+    const formattedDate = moment(date).format('MMM YYYY');
+
+    return (
+      <div className={styles.customHeader}>
+        <div>{formattedDate}</div>
+        <div className={styles.changeMonth}>
+          <IconButton onClick={decreaseMonth}>
+            <UpArrowIcon />
+          </IconButton>
+          <IconButton onClick={increaseMonth}>
+            <DownArrowIcon />
+          </IconButton>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className={styles.eventTime}>
-      <span className={styles.timeItem}>{startDate}</span>
+      <div className={styles.datePickerContainer}>
+        <DatePicker
+          onChange={(date) => {}}
+          customInput={<span className={styles.timeItem}>{startDate}</span>}
+          renderCustomHeader={CustomHeader}
+        />
+      </div>
       <span className={styles.timeItem}>{startTime}</span>
       <span className={styles.timeItem}>{endTime}</span>
-      {startDate !== endDate && <span className={styles.timeItem}>{endDate}</span>}
+      {startDate !== endDate && (
+        <div className={styles.datePickerContainer}>
+          <DatePicker
+            onChange={(date) => {}}
+            customInput={<span className={styles.timeItem}>{endDate}</span>}
+            renderCustomHeader={CustomHeader}
+          />
+        </div>
+      )}
     </div>
   );
 };
